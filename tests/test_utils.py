90946dd2518e0819928a2451e3ab9569829bc5ff
yvictor <yvictor3141@gmail.com>
2022-05-19 22:58:49 +0800

    chore: let price utils testcase more readable

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,25 +1,41 @@
+import pytest
 from sjtrade.utils import price_ceil, price_floor
 
 
-def test_price_ceil():
-    assert price_ceil(0.111) == 0.12
-    assert price_ceil(1.111) == 1.12
-    assert price_ceil(9.999) == 10
-    assert price_ceil(10.0377878) == 10.05
-    assert price_ceil(50.14) == 50.2
-    assert price_ceil(131.444) == 131.5
-    assert price_ceil(998.1) == 999
-    assert price_ceil(1053.1) == 1055
-    assert price_ceil(9981.4) == 9990
+@pytest.mark.parametrize(
+    ("input", "expected"),
+    [
+        (9.989, 9.99),
+        (10.01, 10.05),
+        (50.01, 50.1),
+        (100.01, 100.5),
+        (500.01, 501),
+        (1000.01, 1005),
+        (5000.01, 5010),
+        (0.111, 0.12),
+        (1.111, 1.12),
+        (9.999, 10),
+        (10.0377878, 10.05),
+    ],
+)
+def test_price_ceil(input: float, expected: float):
+    assert price_ceil(input) == expected
 
 
-def test_price_floor():
-    assert price_floor(0.119) == 0.11
-    assert price_floor(1.119) == 1.11
-    assert price_floor(9.999) == 9.99
-    assert price_floor(10.0877878) == 10.05
-    assert price_floor(50.18) == 50.1
-    assert price_floor(131.9894) == 131.5
-    assert price_floor(998.8) == 998
-    assert price_floor(1058.9) == 1055
-    assert price_floor(9989.5) == 9980
+@pytest.mark.parametrize(
+    ("input", "expected"),
+    [
+        (9.999, 9.99),
+        (10.19, 10.15),
+        (50.19, 50.1),
+        (101.9, 101.5),
+        (519.9, 519.0),
+        (1019.9, 1015.0),
+        (5199.9, 5190.0),
+        (0.119, 0.11),
+        (1.119, 1.11),
+        (9.999, 9.99),
+    ],
+)
+def test_price_floor(input: float, expected: float):
+    assert price_floor(input) == expected
