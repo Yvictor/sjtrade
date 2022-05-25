2432b50e931f17debdae5fdcda473912131d279b
yvictor <victor.yang@sinopac.com>
2022-05-25 17:51:19 +0800

    feat: interface ok maybe use rx pattern next version

--- sjtrade/trader.py
+++ sjtrade/trader.py
@@ -1,41 +1,183 @@
+from dataclasses import dataclass, field
+from re import L
 from typing import Dict, List
 import shioaji as sj
 
 from .utils import price_round
 from loguru import logger
-from shioaji.constant import Action, TFTStockPriceType, TFTOrderType, QuoteVersion
+from shioaji.constant import (
+    Action,
+    TFTStockPriceType,
+    TFTOrderType,
+    QuoteVersion,
+    Exchange,
+)
+
+
+@dataclass
+class Position:
+    contract: sj.contracts.Contract
+    quantity: int
+    stop_loss_price: float
+    stop_profit_price: float
+    entry_trades: List[sj.order.Trade] = field(default_factory=list)
+    cancel_quantity: int = 0
+    entry_quantity: int = 0
+    open_quantity: int = 0
+    cover_quantity: int = 0
 
 
 class SJTrader:
     def __init__(self, api: sj.Shioaji):
         self.api = api
+        self.positions = {}
+        self._stop_loss_pct = 0.09
+        self._stop_profit_pct = 0.09
+        self.open_price = {}
+        # self.account = api.stock_account
+        # self.entry_trades: Dict[str, sj.order.Trade] = {}
 
     def start(self):
         pass
 
+    @property
+    def stop_loss_pct(self) -> float:
+        return self._stop_loss_pct
+
+    @stop_loss_pct.setter
+    def stop_loss_pct(self, v: float) -> float:
+        self._stop_loss_pct = v
+
+    @property
+    def stop_profit_pct(self) -> float:
+        return self._stop_profit_pct
+
+    @stop_profit_pct.setter
+    def stop_profit_pct(self, v: float) -> float:
+        self._stop_profit_pct = v
+
     def place_entry_order(
         self, position: Dict[str, int], pct: float
     ) -> List[sj.order.Trade]:
-        for code in position:
-            if not self.api.Contracts.Stocks[code]:
+        trades = []
+        for code, pos in position.items():
+            contract = self.api.Contracts.Stocks[code]
+            if not contract:
                 logger.warning(f"Code: {code} not exist in TW Stock.")
             else:
-                self.api.quote.subscribe(
-                    self.api.Contracts.Stocks[code], version=QuoteVersion.v1
+                # TODO abstract func
+                stop_loss_price = contract.reference * (
+                    1 + (-1 if pos > 0 else 1) * (self._stop_loss_pct)
                 )
-        return [
-            self.api.place_order(
-                contract=self.api.Contracts.Stocks[code],
-                order=sj.Order(
-                    price=price_round(
-                        self.api.Contracts.Stocks[code].reference * pct, pos > 0
+                stop_profit_price = contract.reference * (
+                    1 + (1 if pos > 0 else -1) * (self._stop_profit_pct)
+                )
+                self.positions[code] = dict(
+                    contract=contract,
+                    quantity=position[code],
+                    stop_loss_price=price_round(stop_loss_price, pos > 0),
+                    stop_profit_price=price_round(stop_profit_price, pos < 0),
+                    cancel_quantity=0,
+                    entry_quantity=0,
+                    cover_quantity=0,
+                )
+                self.api.quote.subscribe(contract, version=QuoteVersion.v1)
+                # TODO over 499 need handle
+                trade = self.api.place_order(
+                    contract=self.api.Contracts.Stocks[code],
+                    order=sj.Order(
+                        price=price_round(
+                            self.api.Contracts.Stocks[code].reference * pct, pos > 0
+                        ),
+                        quantity=abs(pos),
+                        action=Action.Buy if pos > 0 else Action.Sell,
+                        price_type=TFTStockPriceType.LMT,
+                        order_type=TFTOrderType.ROD,
                     ),
-                    quantity=abs(pos),
-                    action=Action.Buy if pos > 0 else Action.Sell,
-                    price_type=TFTStockPriceType.LMT,
-                    order_type=TFTOrderType.ROD,
-                ),
-            )
-            for code, pos in position.items()
-            if self.api.Contracts.Stocks[code]
-        ]
+                )
+                trades.append(trade)
+                self.positions[code]["entry_trades"] = [
+                    trade,
+                ]
+        return trades
+    
+
+    def cancel_preorder_handler(self, exchange: Exchange, tick: sj.TickSTKv1):
+        position = self.positions[tick.code]
+        contract = position["contract"]
+        # 8:55 - 8:59:55
+        if tick.simtrade:
+            if position["quantity"] < 0 and tick.close == contract.limit_up:
+                for trade in self.positions[tick.code]["entry_trades"]:
+                    if trade.status.status != sj.order.Status.Cancelled:
+                        self.api.cancel_order(trade)
+                        self.update_status(trade)
+                        if trade.status.status == sj.order.Status.Cancelled:
+                            position["cancel_quantity"] -= trade.status.cancel_quantity
+                        else:
+                            logger.warning("position {} not cancel....")
+                            # TODO handel it
+    
+    def re_entry_order(self, exchange: Exchange, tick: sj.TickSTKv1):
+        position = self.positions[tick.code]
+        contract = position["contract"]
+    
+        # 9:00 -> first
+        if not tick.simtrade:
+            if tick.code not in self.open_price:
+                self.open_price[tick.code] = tick.close
+                if tick.close < position["stop_loss_price"]:
+                    trade = self.api.place_order(
+                        contract=contract,
+                        order=sj.Order(
+                            price=0,
+                            quantity=abs(position["quantity"]),
+                            action=Action.Buy
+                            if position["quantity"] > 0
+                            else Action.Sell,
+                            price_type=TFTStockPriceType.MKT,
+                            order_type=TFTOrderType.ROD,
+                        ),
+                    )
+                    self.update_status(trade)
+
+    def intraday_handler(self, exchange: Exchange, tick: sj.TickSTKv1):
+        position = self.positions[tick.code]
+        # 9:00 -> 13:24:49 stop loss stop profit
+        if not tick.simtrade:
+            if (
+                position["open_quantity"] > 0
+                and tick.close <= position["stop_loss_price"]
+            ):
+                self.place_cover_order(position)
+
+            if (
+                position["open_quantity"] < 0
+                and tick.close >= position["stop_loss_price"]
+            ):
+                self.place_cover_order(position)
+
+            if (
+                position["open_quantity"] > 0
+                and tick.close >= position["stop_profit_price"]
+            ):
+                self.place_cover_order(position)
+
+            if (
+                position["open_quantity"] < 0
+                and tick.close <= position["stop_profit_price"]
+            ):
+                self.place_cover_order(position)
+
+        # 13:26 place cover order
+        self.open_position_cover()
+
+    def place_cover_order(self, position):
+        pass
+    
+    def open_position_cover(self):
+        pass
+
+    def update_status(self, trade: sj.order.Trade) -> sj.order.Trade:
+        self.api._solace.update_status(trade.order.account, seqno=trade.status.seqno)
+        return trade
--- tests/test_trader.py
+++ tests/test_trader.py
@@ -1,10 +1,27 @@
 import pytest
+import datetime
 import loguru
+from pytest_mock import MockerFixture
 import shioaji as sj
 
+from decimal import Decimal
+from dataclasses import dataclass
 from sjtrade.trader import SJTrader
-from pytest_mock import MockerFixture
-from shioaji.constant import Action, TFTStockPriceType, TFTOrderType, QuoteVersion
+from shioaji.constant import (
+    Action,
+    TFTStockPriceType,
+    TFTOrderType,
+    QuoteVersion,
+    Exchange,
+)
+
+
+@dataclass
+class TickSTKv1:
+    code: str
+    datetime: datetime.datetime
+    close: Decimal
+    simtrade: bool
 
 
 @pytest.fixture
@@ -26,6 +43,8 @@ def test_sjtrader_place_entry_order(sjtrader: SJTrader, logger: loguru._logger.L
         contract, order, sj.order.OrderStatus(status=sj.order.Status.PreSubmitted)
     )
     position = {"1605": -1, "6290": -3, "0000": 3}
+    sjtrader.stop_loss_pct = 0.085
+    sjtrader.stop_profit_pct = 0.09
     res = sjtrader.place_entry_order(position, 1.05)
     logger.warning.assert_called_once()
     sjtrader.api.quote.subscribe.assert_has_calls(
@@ -34,8 +53,7 @@ def test_sjtrader_place_entry_order(sjtrader: SJTrader, logger: loguru._logger.L
             ((sjtrader.api.Contracts.Stocks["6290"],), dict(version=QuoteVersion.v1)),
         ]
     )
-    assert len(res) == 2
-    assert res == [
+    expected = [
         sj.order.Trade(
             sjtrader.api.Contracts.Stocks["1605"],
             sj.Order(
@@ -59,3 +77,76 @@ def test_sjtrader_place_entry_order(sjtrader: SJTrader, logger: loguru._logger.L
             status=sj.order.OrderStatus(status=sj.order.Status.PreSubmitted),
         ),
     ]
+    assert sjtrader.positions == {
+        "1605": dict(
+            contract=sjtrader.api.Contracts.Stocks["1605"],
+            quantity=-1,
+            stop_loss_price=42.7,
+            stop_profit_price=35.9,
+            cancel_quantity=0,
+            entry_quantity=0,
+            cover_quantity=0,
+            entry_trades=[
+                expected[0],
+            ],
+        ),
+        "6290": dict(
+            contract=sjtrader.api.Contracts.Stocks["6290"],
+            quantity=-3,
+            stop_loss_price=62.1,
+            stop_profit_price=52.2,
+            cancel_quantity=0,
+            entry_quantity=0,
+            cover_quantity=0,
+            entry_trades=[
+                expected[1],
+            ],
+        ),
+    }
+    assert len(res) == 2
+    assert res == expected
+
+
+def test_sjtrader_cancel_preorder_handler(sjtrader: SJTrader, mocker: MockerFixture):
+    # tick = sj.TickSTKv1.from_content("1605", "2022-05-25", 39.5, 39, 39.4,)
+    position = {"1605": -1, "6290": -3, "0000": 3}
+    tick = TickSTKv1("1605", "2022-05-25 08:45:01", 43.3, True)
+    sjtrader.api.place_order.side_effect = lambda contract, order: sj.order.Trade(
+        contract, order, sj.order.OrderStatus(status=sj.order.Status.PreSubmitted)
+    )
+
+    def make_cancel_order_status(trade):
+        trade.status.status = sj.order.Status.Cancelled
+        trade.status.cancel_quantity = trade.order.quantity
+
+    sjtrader.update_status = mocker.MagicMock(side_effect=make_cancel_order_status)
+
+    sjtrader.place_entry_order(position, 1.05)
+    sjtrader.cancel_preorder_handler(Exchange.TSE, tick)
+    sjtrader.api.cancel_order.assert_called_once_with(
+        sjtrader.positions["1605"]["entry_trades"][0]
+    )
+    # TODO need single trade update status interface
+    sjtrader.update_status.assert_called_once_with(
+        sjtrader.positions["1605"]["entry_trades"][0]
+    )
+    # sjtrader.api._solace.update_status.assert_called()
+    assert sjtrader.positions["1605"]["cancel_quantity"] == -1
+
+
+def test_sjtrader_re_entry_order(sjtrader: SJTrader):
+    tick = TickSTKv1("1605", "2022-05-25 08:45:01", 43.3, True)
+    sjtrader.re_entry_order(Exchange.TSE, tick)
+
+
+def test_sjtrader_intraday_handler(sjtrader: SJTrader):
+    tick = TickSTKv1("1605", "2022-05-25 08:45:01", 43.3, True)
+    sjtrader.intraday_handler(Exchange.TSE, tick)
+
+
+def test_sjtrader_place_cover_order(sjtrader: SJTrader):
+    sjtrader.place_cover_order({})
+
+
+def test_sjtrader_open_position_cover(sjtrader: SJTrader):
+    sjtrader.open_position_cover()
